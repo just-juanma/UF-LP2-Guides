6d4ceb07fb5f95816b93974a8a9dbb518faf3554
Shydes <jugoka947@gmail.com>
2022-08-05 20:04:29 -0300

    Ejercicio 3, 4 y 5. Faltan comentarios.

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -12,8 +12,16 @@ namespace Recursion
         {
             int n = 5; // cambiar este numero por el que quieras saber su cantidad de digitos
             int n2 = 784;
+            string str = "Josefina Herrera Juan Manuel Pampa Team Lorenzo Alegre";
+            string neuquen = "neuquen";
+            string nose = "Me gustan las verduras";
             Console.WriteLine("La cantidad de digitos del numero " + n + " es: "+ nDigitos(n));
             Console.WriteLine("Inversa de " + n2 + " es: " + invertirRecursivo(n2));
+            Console.WriteLine("Inversa de " + str + " es: " + invertirString(str));
+            Console.WriteLine("La palabra " + neuquen);
+            if (isPalindromo(neuquen)) Console.WriteLine(" es palindromo");
+            else Console.WriteLine(" no es palindromo");
+            Console.WriteLine("La cantidad de vocales es: " + nVocales(nose));
         }
         /// <summary>
         /// Recursion de la cantidad de digitos de un numero. (seguir)
@@ -34,6 +42,51 @@ namespace Recursion
                 return (int)(Math.Pow(10, temp - 1)) * (n % 10) + invertirRecursivo(n / 10);                      
         }
 
-        //static string invertirString()
+        static string invertirString(string cadena) {
+            int temp = cadena.Length - 1;
+            if (temp == 0) return cadena;
+            else
+                return cadena[temp] + invertirString(cadena.Remove(temp));
+
+        }
+
+        /// <summary>
+        /// No es recursion realmente (paja)
+        /// </summary>
+        /// <param name="cadena"></param>
+        /// <returns></returns>
+        static bool isPalindromo(string cadena)
+        {
+            int temp = cadena.Length - 1;
+            if (temp == 1 || temp == 0) return true;
+            else
+                return String.Equals(cadena, invertirString(cadena));
+        }
+
+        /// <summary>
+        /// SOLO CONSIDERAMOS MAYUSCULAS Y MINUSCULAS, NO TILDES NI NADA RARO EN ESPAñOL.. SALUDOS!!!!
+        /// </summary>
+        /// <param name="cadena"></param>
+        /// <returns></returns>
+        static uint nVocales(string cadena)
+        {
+            int temp = cadena.Length - 1, cont = 0;
+            if (temp == 0) return (uint)cont;
+            else
+            {
+                if     (cadena[temp] == 'a' ||
+                        cadena[temp] == 'e' ||
+                        cadena[temp] == 'i' ||
+                        cadena[temp] == 'o' ||
+                        cadena[temp] == 'u' ||
+                        cadena[temp] == 'A' ||
+                        cadena[temp] == 'E' ||
+                        cadena[temp] == 'I' ||
+                        cadena[temp] == 'O' ||
+                        cadena[temp] == 'U') cont++;
+                return (uint)cont + nVocales(cadena.Remove(temp));
+            }
+        }
+
     }
 }
